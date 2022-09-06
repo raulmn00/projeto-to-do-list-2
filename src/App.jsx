d4941e723934863361930f59d34fb8be53f558d8
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-09-05 21:38:14 -0300

    feat: create project and repo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className="container-to-do-list">
+				<div className="header-to-do-list">
+					<p>Header</p>
+				</div>
+				<div className="main-content">
+					<p>Conteudo Principal</p>
+				</div>
+				<div className="footer">
+					<p>Footer</p>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
